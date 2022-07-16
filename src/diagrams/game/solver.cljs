f568774fadaf7675a6d6c700e8db9ea08fdc3eef
spike <bank.tellr@gmail.com>
2022-07-16 22:54:37 +0900

    Implements `deadends-are-mobs`

--- src/diagrams/game/solver.cljs
+++ src/diagrams/game/solver.cljs
@@ -1,6 +1,11 @@
 (ns diagrams.game.solver
-  (:require [diagrams.matrix :refer (h-count v-count)]))
+  (:require [diagrams.matrix :refer (h-count v-count get-dim)]))
 
+
+(defn- valid-forms? [game walls]
+  ; (dim game) = (dim walls)
+  ; every corr. mask-elem != :empty -> false? walls
+  false)
 
 (defn- walls-satisfied? [game walls]
   (let [h-hints (:h-hints game)
@@ -11,14 +16,30 @@
     (and (= hc h-hints)
          (= vc v-hints))))
 
-;(defn- safe-get [game x y]
-;  nil)
+(defn- safe-get-wall [walls x y]
+  (get-in walls [x y] true))
+
+(defn- get-neighbors [mat x y default-val]
+  [(get-in mat [(+ x 1) y      ] default-val)
+   (get-in mat [(- x 1) y      ] default-val)
+   (get-in mat [x       (+ y 1)] default-val)
+   (get-in mat [x       (- y 1)] default-val)])
 
 ;(defn- matches-pattern? [game walls x y pattern]
 ;  false)
 
-;(defn- deadends-are-mobs [game walls]
-;  false)
+(defn- deadends-are-mobs [game walls]
+  (let [mask  (:mask game)
+        [w h] (get-dim mask)]
+    (every? true?
+            (for [x (range w)
+                  y (range h)
+                  :let [mask-elem (get-in mask [x y])
+                        ;wall      (get-in walls [x y])
+                        wall-neighbors (get-neighbors walls x y true)]]
+              (if (= mask-elem :mob)
+                (= (count (filter true? wall-neighbors)) 3)
+                true)))))
 ;
 ;(defn- chest-in-room? [game walls]
 ;  false)
@@ -30,8 +51,9 @@
 ;    false))
 
 (defn solved? [game walls]
-  (and (walls-satisfied? game walls)
-       ;(deadends-are-mobs game walls)
+  (and true
+       (walls-satisfied? game walls)
+       (deadends-are-mobs game walls)
        ;(chest-in-room? game walls)
        ;(paths-narrow? game walls)
        ;(chest-in-room? game walls)
