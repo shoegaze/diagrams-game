73c289d55ed3522dad0bd1ea0c696cb364729a66
spike <bank.tellr@gmail.com>
2022-07-16 22:54:13 +0900

    Updates `.gitignore`

--- resources/public/js/bundle/diagrams/ui/row.cljs
+++
@@ -1,9 +0,0 @@
-(ns diagrams.ui.row
-  (:require [diagrams.ui.cell :refer (cell-component)]))
-
-
-(defn row-component [game walls markers n x]
-  [:div.row
-     (for [y (range n)
-           :let [key (+ (* n y) x)]]
-       ^{:key key} [cell-component game walls markers x y])])
