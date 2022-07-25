4805d65977e10ee3f49288c52b2bc98b796f009c
spike <bank.tellr@gmail.com>
2022-07-25 10:27:22 +0900

    FIX: Typo

--- src/diagrams/game/solver.cljs
+++ src/diagrams/game/solver.cljs
@@ -52,7 +52,7 @@
        ;(valid-forms? game walls)
        (walls-satisfied? game walls)
        (dead-ends-are-mobs game walls)
-       (chests-in-room? game walls)
+       (chests-in-rooms? game walls)
        ;(paths-narrow? game walls)
        ;(paths-connected game walls)
        ))
